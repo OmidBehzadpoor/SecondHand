42c26dcee801c3fbfd9cfb8d853c8cdd7eca5bd3
Parsa Zebardast <parsazzce@gmail.com>
2026-07-04 17:35:55 +0330

    refactor(repository/AdvertisementRepository): align AdvertisementRepository with other repositories

--- backend/src/main/java/com/example/secondhand/repository/AdvertisementRepository.java
+++ backend/src/main/java/com/example/secondhand/repository/AdvertisementRepository.java
@@ -2,11 +2,9 @@ package com.example.secondhand.repository;
 
 import com.example.secondhand.model.Advertisement;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface AdvertisementRepository extends JpaRepository<Advertisement, Long> {
 
 }
