a98ee39e7ef5abc9e8bfbe32bd4d0f74f011d2ae
Parsa Zebardast <parsazzce@gmail.com>
2026-07-20 19:33:57 +0330

    fix(frontend/MainApplication): reapply login view and show()

--- frontend/src/main/java/com/example/secondhandfx/MainApplication.java
+++ frontend/src/main/java/com/example/secondhandfx/MainApplication.java
@@ -2,9 +2,6 @@ package com.example.secondhandfx;
 
 import com.example.secondhandfx.util.SceneNavigator;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class MainApplication extends Application {
@@ -12,14 +9,7 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage primaryStage) {
         SceneNavigator.setPrimaryStage(primaryStage);
-
-        Label welcomeLabel = new Label("به بهترین سامانه خرید و فروش دست دوم دنیا که توسط امید و پارسا طراحی شده است خوش آمدید");
-
-        StackPane root = new StackPane(welcomeLabel);
-        Scene scene = new Scene(root, 800, 600);
-
-        primaryStage.setTitle("SecondHand Market");
-        primaryStage.setScene(scene);
+        SceneNavigator.navigateTo("/com/example/secondhandfx/fxml/login.fxml", "ورود");
         primaryStage.show();
     }
 
