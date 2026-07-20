c375a538ab759058b5019551f64b7e1ee6e3490c
Omid Behzadpoor <omidbehzadpoor1386@gmail.com>
2026-07-20 21:46:29 +0330

    feat(model): add CityResponse DTO

---
+++ frontend/src/main/java/com/example/secondhandfx/model/CityResponse.java
@@ -0,0 +1,13 @@
+package com.example.secondhandfx.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CityResponse {
+    private Long id;
+    private String name;
+}
