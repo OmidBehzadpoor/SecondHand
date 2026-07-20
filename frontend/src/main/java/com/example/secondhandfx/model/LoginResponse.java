ef74a527fe67f3fadea1d3cfbb27f45bec991347
Omid Behzadpoor <omidbehzadpoor1386@gmail.com>
2026-07-20 14:16:17 +0330

    feat(model): add LoginResponse DTO

---
+++ frontend/src/main/java/com/example/secondhandfx/model/LoginResponse.java
@@ -0,0 +1,19 @@
+package com.example.secondhandfx.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginResponse {
+    private String token;
+    private Long userId;
+    private String username;
+    private Role role;
+}
