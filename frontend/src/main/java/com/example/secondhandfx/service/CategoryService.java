48fb8bc799aee791d249a6f7ceeceb16eb239c2c
Omid Behzadpoor <omidbehzadpoor1386@gmail.com>
2026-07-21 14:50:53 +0330

    feat(category-service): add update/activate/deactivate methods

--- frontend/src/main/java/com/example/secondhandfx/service/CategoryService.java
+++ frontend/src/main/java/com/example/secondhandfx/service/CategoryService.java
@@ -9,5 +9,8 @@ public interface CategoryService {
     List<CategoryResponse> getAllCategories() throws ApiException;
     List<CategoryResponse> getAllCategoriesForAdmin() throws ApiException;
     CategoryResponse createCategory(String name, Long parentId) throws ApiException;
+    CategoryResponse updateCategory(Long id, String name, Long parentId) throws ApiException;
+    CategoryResponse activateCategory(Long id) throws ApiException;
+    CategoryResponse deactivateCategory(Long id) throws ApiException;
     void deleteCategory(Long id) throws ApiException;
 }
