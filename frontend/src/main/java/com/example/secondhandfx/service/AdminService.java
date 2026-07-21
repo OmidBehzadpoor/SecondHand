242fb2b26ba08d14c9123f03791b431611182e8f
Omid Behzadpoor <omidbehzadpoor1386@gmail.com>
2026-07-21 14:39:33 +0330

    feat(service): add getDashboard to AdminService interface

--- frontend/src/main/java/com/example/secondhandfx/service/AdminService.java
+++ frontend/src/main/java/com/example/secondhandfx/service/AdminService.java
@@ -2,11 +2,14 @@ package com.example.secondhandfx.service;
 
 import com.example.secondhandfx.exception.ApiException;
 import com.example.secondhandfx.model.AdminAdvertisementResponse;
+import com.example.secondhandfx.model.AdminDashboardResponse;
 import com.example.secondhandfx.model.AdminUserResponse;
 
 import java.util.List;
 
 public interface AdminService {
+
+    AdminDashboardResponse getDashboard() throws ApiException;
 
     List<AdminAdvertisementResponse> getPendingAdvertisements() throws ApiException;
 
